4b83b6a285a1da34ab51d2fb4ae5d84cafdfd837
Pedro Felix <pereraflix15@gmail.com>
2023-03-03 16:36:24 -0300

    Criado mddleware de autenticação

--- src/modules/post/postController.js
+++ src/modules/post/postController.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
+import { verifyAccessToken } from "../../utils/auth";
 
 const router = Router()
 
-router.post('/post', (req, res) => {
-  res.send('CREATE POST')
+router.post('/', verifyAccessToken, (req, res) => {
+  res.send('CREATE POST /')
 })
 
-router.get('/:id?', (req, res) => {
+router.get('/:id?', verifyAccessToken, (req, res) => {
   //DUAS OPÇÕES: LISTAR TODOS OU APENAS UM
-  res.send('GET POST')
+  res.send('GET POST /')
 })
 
 export default router
