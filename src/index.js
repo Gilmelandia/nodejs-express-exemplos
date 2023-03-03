467cd50b83865cc43dadee6380d4b8a7615e3f1f
Pedro Felix <pereraflix15@gmail.com>
2023-03-03 15:28:45 -0300

    criado login e autenticação com JWT

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import * as express from 'express'
+import * as cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
 import postController from './modules/post/postController'
 import userController from './modules/user/userController'
